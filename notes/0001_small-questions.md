ff211e908540033a91f19bef91803eb69935af29
Paul Scharnofske <asynts@gmail.com>
2023-01-28 14:39:49 +0100

    Work: Clean up the interface of argparse library
    This is already looking much better but there are still a few things I
    need to figure out:
    
    -   How can my implementation report errors?
    
        Maybe add custom '.print_help' action?
    
    -   How does the parsing algorithm function?
    
        Maybe create unit tests in advance?

--- notes/0001_small-questions.md
+++ notes/0001_small-questions.md
@@ -1,3 +1,5 @@
 ### Open
 
 -   How to use `std.debug.print` with a boolean?
+
+-   How to access the last element of `std.ArrayList`?
--- src/argument_parser.zig
+++ src/argument_parser.zig
@@ -1,62 +1,164 @@
 const std = @import("std");
 
-pub const ArgumentType = enum {
-    // FIXME: I don't think this should be called 'boolean', maybe 'flag'?
+// FIXME: Find a better name for this library.
+
+pub const ValueEnum = enum {
     boolean,
     string,
 };
-
-pub const Argument = struct {
-    name: []const u8,
-    short_name: ?u8,
-    type_: ArgumentType,
+pub const Value = union(ValueEnum) {
+    boolean: bool,
+    string: []const u8,
 };
 
-pub const ParseOutput = struct {
+// FIXME: Find a better name for this.
+pub const Namespace = struct {
     const Self = @This();
 
-    // FIXME: I want something like python where you have a key and value.
-    //        Thus multiple options can write the same value?
-    flags: std.StringHashMap(bool),
-    strings: std.StringHashMap([]const u8),
+    values: std.StringHashMap(Value),
 
-    // FIXME: I don't like unwrapping the value here.
-    pub fn isFlagSet(self: *const Self, name: []const u8) !bool {
-        return self.flags.get(name).?;
-    }
-
-    pub fn getStringValue(self: *const Self, name: []const u8) ?[]const u8 {
-        return self.strings.get(name);
-    }
-};
-
-pub const ArgumentParser = struct {
-    const Self = @This();
-
-    // FIXME: This is suboptimal.
-    //        Everything should be an argument.
-    //        We must ensure that there are no duplicates.
-    arguments: std.StringHashMap(Argument),
-    positional_arguments: std.ArrayList([]const u8),
-
-    pub fn init() Self {
-        ArgumentParser{};
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return Self{
+            .values = std.StringHashMap(Value).init(allocator),
+        };
     }
     pub fn deinit(self: *Self) void {
-        _ = self;
+        self.values.deinit();
     }
 
-    pub fn add_argument(self: *Self, argument: Argument) !void {
-        try self.arguments.put(argument.long_name, argument);
+    pub fn declare(self: *Self, key: []const u8, default_value: Value) !void {
+        try self.values.put(key, default_value);
     }
 
-    pub fn add_positional_argument(self: *Self, name: []const u8) !void {
-        try self.positional_arguments.append(name);
+    pub fn isFlagSet(self: *const Self, key: []const u8) !bool {
+        switch (self.values.get(key).?) {
+            .boolean => |value| return value,
+            else => return error.UnexpectedUnionVariant,
+        }
     }
 
-    pub fn parse(self: *const Self) !ParseOutput {
+    pub fn get(comptime T: type, self: *const Self, key: []const u8) !T {
+        switch (self.values.get(key).?) {
+            .string => |value| {
+                if (T == []const u8) {
+                    return value;
+                }
+            },
+            .boolean => |value| {
+                if (T == bool) {
+                    return value;
+                }
+            },
+        }
+
+        return error.UnexpectedUnionVariant;
+    }
+};
+
+pub const ActionEnum = enum {
+    store_true,
+    store_false,
+    store_string,
+};
+
+const Option = struct {
+    key_name: []const u8,
+    action: ActionEnum,
+
+    long_name: ?[]const u8,
+    short_name: ?u8,
+};
+const PositionalArgument = struct {
+    key_name: []const u8,
+    action: ActionEnum,
+};
+
+pub const Parser = struct {
+    const Self = @This();
+
+    // There are multiple 'Option' objects representing the same option.
+    options: std.ArrayList(Option),
+    long_options: std.StringHashMap(Option),
+    short_options: std.StringHashMap(Option),
+
+    positional_arguments: std.ArrayList(),
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        // FIXME: Add help option here.
+        //        Use a special action to print help page.
+
+        return Self{
+            .options = std.ArrayList(Option).init(allocator),
+            .long_options = std.StringHashMap(void).init(allocator),
+            .short_options = std.StringHashMap(void).init(allocator),
+        };
+    }
+    pub fn deinit(self: *Self) void {
+        self.options.deinit();
+        self.long_options.deinit();
+        self.short_options.deinit();
+    }
+
+    pub fn add_option(
+        self: *Self,
+        key_name: []const u8,
+        action: ActionEnum,
+        long_name: ?[]const u8,
+        short_name: ?u8) !void
+    {
+        std.debug.assert(long_name or short_name);
+
+        switch (action) {
+            .store_true,
+            .store_false,
+            .store_string => {},
+        }
+
+        var option = Option{
+            .key_name = key_name,
+            .action = action,
+            .long_name = long_name,
+            .short_name = short_name,
+        };
+
+        if (long_name) |name| {
+            self.long_options.put(name, option).?;
+        }
+        if (short_name) |name| {
+            self.short_options.put(name, option).?;
+        }
+        try self.options.append(option);
+    }
+
+    pub fn add_positional_argument(self: *Self, key_name: []const u8, action: ActionEnum) !void {
+        switch (action) {
+            .store_true,
+            .store_false => @panic("action not supported for positional arguments"),
+
+            .store_string,
+            .store_string_list => {},
+        }
+
+        if (self.positional_arguments.len >= 1) {
+            switch (std.debug.assert(self.positional_arguments[self.positional_arguments.len - 1])) {
+                .store_string_list => @panic("previous positional argument already captured all remaining elements"),
+                else => {},
+            }
+        }
+
+        try self.positional_arguments.append(PositionalArgument{
+            .key_name = key_name,
+            .action = action,
+        });
+    }
+
+    // FIXME: How can I report errors with this API?
+    //        Maybe add an 'parse_impl' method that takes a buffer writer object.
+    //        Then there is a normal 'parse(self: *const Self) !Namespace' method.
+    pub fn parse(self: *const Self, argv: [][]const u8) !Namespace {
         // FIXME
         _ = self;
+        _ = argv;
         @panic("not implemented");
     }
 };
