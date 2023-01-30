44f2ec00569bb0660e8d6cf48f52a58dc297a1aa
Paul Scharnofske <asynts@gmail.com>
2023-01-30 10:36:09 +0100

    Template: Implement outline for 'Generator.run'
    The big problem is, that I need to get the field name at compile time.
    I don't know how to do that yet.  I should look at 'std.fmt.format' for
    inspiration.

--- src/asynts_template/lib.zig
+++ src/asynts_template/lib.zig
@@ -2,6 +2,50 @@ const std = @import("std");
 
 const expect = std.testing.expect;
 
+pub const Lexer = struct {
+    const Self = @This();
+
+    input: []const u8,
+    offset: usize,
+
+    pub fn init(input: []const u8) Self {
+        return Self{
+            .input = input,
+            .offset = 0,
+        };
+    }
+
+    pub fn isEnd(self: *const Self) bool {
+        return self.offset >= self.input.len;
+    }
+
+    pub fn consumeUntil(self: *Self, marker: u8) []const u8 {
+        var start_offset = self.offset;
+
+        while (!self.isEnd()) {
+            if (self.peek() == marker) {
+                return self.input[start_offset..self.offset];
+            }
+            self.offset += 1;
+        }
+
+        return self.input[start_offset..];
+    }
+
+    pub fn consumeChar(self: *Self, char: u8) bool {
+        if (self.isEnd()) {
+            return false;
+        }
+
+        if (self.input[self.offset] == char) {
+            self.offset += 1;
+            return true;
+        }
+
+        return false;
+    }
+};
+
 const Generator = struct {
     const Self = @This();
 
@@ -19,12 +63,32 @@ const Generator = struct {
     // FIXME: Can we offload the parsing to compile time?
     //        Essentially, this should be a sequence of 'TextPlacementInstruction' and 'VariablePlacementInstruction'.
     //        Generating that at compile time should be possible?
-    pub fn run(self: *const Self, arguments: anytype, inputTemplate: []const u8) ![]const u8 {
-        _ = arguments;
+    pub fn run(self: *const Self, arguments: anytype, comptime inputTemplate: []const u8) ![]const u8 {
+        var lexer = Lexer.init(inputTemplate);
 
-        // FIXME: Actually parse the input string.
+        var buffer = std.ArrayList(u8).init(self.allocator);
+        defer buffer.deinit();
 
-        return self.allocator.dupe(u8, inputTemplate);
+        var writer = buffer.writer();
+
+        while (!lexer.isEnd()) {
+            var rawString = lexer.consumeUntil('{');
+            try writer.print("{s}", rawString);
+
+            if (lexer.consumeChar('{')) {
+                var variableName = lexer.consumeUntil('}');
+
+                if (!lexer.consumeChar('}')) {
+                    return error.SyntaxError;
+                }
+
+                // FIXME: How can I get 'variableName' at compile time here?
+                //        Look at how 'std.fmt.format' solves this issue.
+                try writer.print("{s}", @field(arguments, variableName));
+            }
+        }
+
+        return buffer.toOwnedSlice();
     }
 };
 
@@ -52,3 +116,32 @@ test {
         \\
     ));
 }
+
+test {
+    var allocator = std.testing.allocator;
+
+    var generator = Generator.init(allocator);
+    defer generator.deinit();
+
+    var template = generator.run(
+        .{
+            .text = "Hi!"
+        },
+        \\<div class="Page">
+        \\  <p>{text}</p>
+        \\</div>
+        \\
+    ) catch unreachable;
+    defer allocator.free(template);
+
+    try expect(std.mem.eql(
+        u8,
+        template,
+        \\<div class="Page">
+        \\  <p>Hi!</p>
+        \\</div>
+        \\
+    ));
+}
+
+// FIXME: Test: escape context dependent.
